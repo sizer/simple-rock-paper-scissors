b92e435f5c95a2441c4584d10f66a85d6e2dc163
mrdShinse <shinse1128@hotmail.com>
2018-03-13 01:18:16 +0900

    config serverless.yml

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ var Alexa = require('alexa-sdk');
 
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
 //Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
-var APP_ID = undefined;
+var APP_ID = process.env.ALEXA_APP_ID;
 
 var SKILL_NAME = "豆知識";
 var GET_FACT_MESSAGE = "知ってましたか？";
